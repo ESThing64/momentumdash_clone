2afbdee38b9ccccdc49ed6ade3e29d6afbcbdef9
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-11-17 21:13:52 -0600

    stock api is working

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,59 @@
-let imgAuthEl = document.getElementById("img-auth")
-let backgroundUrl = "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
-let wrongUrl = "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=naturfghfghe"
+const imgAuthEl = document.getElementById("img-auth")
+const backgroundUrl = "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=meditation"
+const wrongUrl = "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=naturfghfghe"
 
+const stockEl = $('#stock')
+let stockUrl
 
 fetch(backgroundUrl)
-    .then(function (response) {
+    .then(function(response) {
         return response.json()
-        .then(function (data) {
+            .then(function(data) {
 
-            document.body.style.backgroundImage = `url(${data.urls.full})`
-            imgAuthEl.textContent = "By: " + data.user.name 
-            
-        })
+                document.body.style.backgroundImage = `url(${data.urls.full})`
+                imgAuthEl.textContent = "By: " + data.user.name
+
+            })
     })
-    .catch(function () {
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
+    .catch(function() {
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1582177199344-a05724b6e775?crop=entropy&amp;cs=srgb&amp;fm=jpg&amp;ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzcyMDA3MDM&amp;ixlib=rb-1.2.1&amp;q=85&quot)`
     })
 
+    fetch(stockUrl)
+    .then(function(response) {
+        return response.json()
+            .then(function(data) {
+
+                if (data.status != "OK"){
+                    throw Error("Something weird happend. Please try again or something")
+                }
 
 
+                stockEl.html(`
+                <p>${data.symbol}</p>
+                <p>High: ${data.high}</p>
+                <p>Low: ${data.low}</p>
+                `)
+
+              console.log(data)
+
+            })
+    })
+    .catch(err => console.error(err))
+   
+    // $.ajax({
+    //     url: backgroundUrl,
+    //     method: 'GET',
+    //   }).then(function (response) {
+    //     return response.json()
+    //     .then(function(data) {
+
+    //         document.body.style.backgroundImage = `url(${data.urls.full})`
+    //         imgAuthEl.textContent = "By: " + data.user.name
+
+    //     })
+        
+
+
+    //   });
+      
