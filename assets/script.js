485fed6e595f7a9107777b55cc9a31b69c355f46
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-11-19 21:16:47 -0600

    finshed the settings feature

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,40 @@
 const imgAuthEl = document.getElementById("img-auth")
-const backgroundUrl = "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=meditation"
+let backgroundUrl = "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=banana"
 const wrongUrl = "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=naturfghfghe"
-
+const stockUrl = "https://api.polygon.io/v1/open-close/AAPL/2020-10-14?adjusted=true&apiKey=386U_EAyyi_HmpaTSQPXvjYIBTSLU_Js"
 const timeNow = luxon.DateTime.local().toFormat("hh:mm a");
 const timeEl = $('#time')
 const stockEl = $('#stock')
 const city = "Houston"
-
+let theme = "nature"
 const cityEl = $('#city');
 const iconEl = $('#icon')
 const tempTEl = $('#temp');
 const humTEl = $('#hum');
 
+let lat;
+let lon;
+
+ backgroundUrl = "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=" + theme 
 
 timeEl.text(timeNow)
-let stockUrl
 
+// let stockUrl
+function getBackground(theme){
+
+    const storedTheme = JSON.parse(localStorage.getItem("theme"));
+
+    if (storedTheme !== null) {
+        theme = storedTheme;
+      }
+
+    backgroundUrl = "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=" + theme 
 fetch(backgroundUrl)
     .then(function(response) {
         return response.json()
             .then(function(data) {
+
+                
 
                 document.body.style.backgroundImage = `url(${data.urls.full})`
                 imgAuthEl.textContent = "By: " + data.user.name
@@ -27,51 +42,25 @@ fetch(backgroundUrl)
             })
     })
     .catch(function() {
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1582177199344-a05724b6e775?crop=entropy&amp;cs=srgb&amp;fm=jpg&amp;ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzcyMDA3MDM&amp;ixlib=rb-1.2.1&amp;q=85&quot)`
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1605666807892-8c11d020bede?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzcyODcwMTY&ixlib=rb-1.2.1&q=85)`
     })
-
+}
     fetch(stockUrl)
     .then(function(response) {
         return response.json()
             .then(function(data) {
-
+                console.log(data)
                 if (data.status != "OK"){
                     throw Error("Something weird happend. Please try again or something")
                 }
-
-
                 stockEl.html(`
                 <span>  ${data.symbol}</span>
                 <span>High: ${data.high}</span>
                 <span>Low: ${data.low}</span>
                 `)
-
-              console.log(data)
-
             })
     })
-    .catch(err => console.error(err))
-   
-    // $.ajax({
-    //     url: backgroundUrl,
-    //     method: 'GET',
-    //   }).then(function (response) {
-    //     return response.json()
-    //     .then(function(data) {
-
-    //         document.body.style.backgroundImage = `url(${data.urls.full})`
-    //         imgAuthEl.textContent = "By: " + data.user.name
-
-    //     })
-        
-
-
-    //   });
-      
-
-
-    // weather
-
+    .catch(err => console.error(err)) 
 
   function getWeatherByCity(){
       
@@ -82,49 +71,72 @@ fetch(backgroundUrl)
         }).then(function(response) {
             const lat = response.coord.lat
             const lon = response.coord.lon
-            const cityName = response.name + "(" + timeNow.toLocaleString() + ")"
-           
-
+                  
             getWeatherDatabyLatlon(lat, lon)
         });
     }
     
     
     function getWeatherDatabyLatlon(lat, lon) {
+
+       
         const weatherDatabyLatlon = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=569b83e02c16eca2296eee261eebaa02"
         $.ajax({
             url: weatherDatabyLatlon,
             method: 'GET',
         }).then(function(response) {
+            console.log(response)
            
             const temp = response.current.temp;
-            const wind = response.current.wind_speed;
             const humidity = response.current.humidity;
-            const uv = response.current.uvi;
             const icont = "http://openweathermap.org/img/wn/" + response.daily[0].weather[0].icon + "@2x.png"
     
             iconEl.attr("src", icont)
             tempTEl.text("Temp: " + temp + "°F");
             humTEl.text("Humidity: " + humidity + "%");
-
-    
-            // for (i = 1; i < 6; i++) {
-    
-            //     let icon1 = "http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png"
-            //     $("#temp" + i).text("Temp:" + response.daily[i].temp.max + "°F");
-            //     $("#hum" + i).text("Humidity:" + response.daily[i].humidity + "%");
-            //     $("#wind" + i).text('Wind: "' + response.daily[i].wind_speed + " MPH")
-            //     $('#icon' + i).attr("src", icon1)
-            //     $('#day' + i).text(timeNow.plus({ days: i }).toLocaleString())
-    
-    
-            // }
-    
-    
-    
+            cityEl.text(response.name)
         });
     
     }
+
+    function getCityByLatLon(){                                  
+        const getCityApi = "http://api.openweathermap.org/geo/1.0/reverse?lat=" + lat + "&lon=" + lon + "&limit=1&appid=569b83e02c16eca2296eee261eebaa02"
+        $.ajax({
+            url: getCityApi,
+            method: 'GET',
+        }).then(function(response) {
+            console.log(response)
+            cityEl.text(response[0].name)
+        });
+    }
+
+    $('#settings-btn').on('click', async function () {
+
+            
+        const themeUpdate = $('#theme').val()
+        localStorage.setItem("theme", JSON.stringify(themeUpdate));
+      
+        console.log(theme)
+        getBackground(themeUpdate)
+        
+
+      })
     
 
-    getWeatherByCity()
+    function getWeatherByCityorGeo() {
+
+    navigator.geolocation.getCurrentPosition(position => {
+        
+        lat = position.coords.latitude
+        lon = position.coords.longitude
+        getCityByLatLon()
+        getWeatherDatabyLatlon(lat, lon)
+    });
+    getWeatherByCity() 
+
+    }
+    
+
+
+getWeatherByCityorGeo()
+getBackground()
